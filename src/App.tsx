16fddd6cdac519023c42d40b4cc0e6e2e1189b21
Maciej <s22482@pjwstk.edu.pl>
2023-02-12 19:54:55 +0100

    Add global colors and create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import React from "react";
+import Header from "./components/fragments/Header";
+import Footer from "./components/fragments/Footer";
 
 function App() {
-  return <div className="App">
-    elo 2
-  </div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <img src="/assets/image.png" />
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;

024fcc56c243005345f1ab16eaa8ca7ba347fd00
Maciej <s22482@pjwstk.edu.pl>
2023-02-13 19:07:56 +0100

    Replace mocked data with graphql query

--- src/components/episodes/EpisodesList.tsx
+++ src/components/episodes/EpisodesList.tsx
@@ -13,84 +13,32 @@ interface EpisodeData {
 }
 
 export default function EpisodesList() {
-  const [episodes, setEpisodes] = useState<any[]>([
-    {
-      episode: "S04E01",
-      name: "Edge of Tomorty: Rick, Die, Rickpeat",
-      air_date: "10.11.2019",
-    },
-    {
-      episode: "S04E02",
-      name: "The Old Man and the Seat",
-      air_date: "17.11.2019",
-    },
-    {
-      episode: "S04E03",
-      name: "One Crew Over the Crewcoo’s Morty",
-      air_date: "24.11.2019",
-    },
-    {
-      episode: "S04E04",
-      name: "Claw and Hoarder: Special Ricktim’s Morty",
-      air_date: "08.12.2019",
-    },
-    {
-      episode: "S04E05",
-      name: "Rattlestar Ricklactica",
-      air_date: "15.12.2019",
-    },
-    {
-      episode: "S04E06",
-      name: "Never Ricking Morty",
-      air_date: "03.05.2020",
-    },
-    {
-      episode: "S04E07",
-      name: "Promortyus",
-      air_date: "10.03.2020",
-    },
-    {
-      episode: "S04E08",
-      name: "The Vat of Acepisode Episode",
-      air_date: "17.03.2020",
-    },
-    {
-      episode: "S04E09",
-      name: "Childrick of Mort",
-      air_date: "24.03.2020",
-    },
-    {
-      episode: "S04E10",
-      name: "Star Mort: Rickturn of the Jerri",
-      air_date: "31.03.2020",
-    },
-  ]);
-  // const { loading, error, data } = useQuery<EpisodeData>(GET_EPISODES_SEASON_4);
+  const { loading, error, data } = useQuery<EpisodeData>(GET_EPISODES_SEASON_4);
 
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
-  // if (error) {
-  //   return <p>Error: {error.message}</p>;
-  // }
-  // if (!data) {
-  //   return null;
-  // }
+  let content;
 
-  // console.log(data);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+  if (!data) {
+    return null;
+  }
 
   return (
     <div className={styles.list}>
       <ul>
-        {episodes.map((res) => (
+        {data.episodes.results.map((res) => (
           <li key={res.episode}>
-            <h2 className={styles.episodeepisode}>{res.episode}</h2>
+            <h2 className={styles.episodeId}>{res.episode}</h2>
           </li>
         ))}
       </ul>
       <hr></hr>
       <ul className={styles.rightList}>
-        {episodes.map((res) => (
+        {data.episodes.results.map((res) => (
           <Episode props={res} key={res.episode} />
         ))}
       </ul>

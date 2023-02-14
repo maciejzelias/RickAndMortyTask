a6a58936a98d9882ded8879c85bdeefaed6567fb
Maciej <s22482@pjwstk.edu.pl>
2023-02-14 18:36:59 +0100

    Add tests of App rendering proper components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  const client = new ApolloClient({
+    uri: "https://rickandmortyapi.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  it("renders the Header component", () => {
+    const { getByTestId } = render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+    expect(getByTestId("header")).toBeInTheDocument();
+  });
+
+  it("renders the MainContent component", () => {
+    const { getByTestId } = render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+    expect(getByTestId("main-content")).toBeInTheDocument();
+  });
+
+  it("renders the footer component", () => {
+    const { getByTestId } = render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+    expect(getByTestId("footer")).toBeInTheDocument();
+  });
 });
